4d7ee48e2cc5d966e9116a95b78427dc190a5480
ItsAvs <avni.morepatil@gmail.com>
2024-06-23 10:26:07 -0700

    ishan final

--- src/Constants.h
+++ src/Constants.h
@@ -1,16 +1,21 @@
 #ifndef MYLIB_CONSTANTS_H
 #define MYLIB_CONSTANTS_H
 
+using namespace pros;
 //Purpose: Create Global Port Constants for Hardware Connection
 namespace PortConstants
 {
-  //create ports here
+  //constants
   
 }
 
 namespace Objects 
 {
-  //put objects here
+  Motor first (1, v5::MotorGears::green, v5::MotorUnits::rotations);
+  Motor second (2, v5::MotorGears::green, v5::MotorUnits::rotations);
+  Motor third (-3, v5::MotorGears::green, v5::MotorUnits::rotations);
+  Motor fourth (-4, v5::MotorGears::green, v5::MotorUnits::rotations);
+  IMU imu(5);
 
 }
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,27 @@
 #include "main.h"
 #include "Constants.h"
 
+using namespace pros;
+using namespace Objects;
+
 //Runs initialization code. This occurs as soon as the program is started.
 void initialize() {
 	pros::lcd::initialize();
 	pros::lcd::set_text(1, "Hello PROS User!");
 }
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
-void autonomous() {}
+void turn(int degrees){
+	while(!(degrees - 5 < imu.get_heading() < degrees + 5)){
+		first.move(100);
+		second.move(100);
+		third.move(100);
+		fourth.move(100);
+	}
+}
 
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
-void opcontrol() {}
+void move(int rotations){
+	first.move_absolute(rotations, 200);
+	second.move_absolute(rotations, 200);
+	third.move_absolute(-rotations, 200);
+	fourth.move_absolute(-rotations, 200);
+}
